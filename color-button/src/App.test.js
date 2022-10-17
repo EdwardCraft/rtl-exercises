a806a63cd8197000a2292b0d8f0c95472c4269be
EdwardCraft <eduardo.flores81@uabc.edu.mx>
2022-10-16 21:30:46 -0700

    Test and code checkbox functinality

--- color-button/src/App.test.js
+++ color-button/src/App.test.js
@@ -18,3 +18,29 @@ test("button turns blue when  clicked", () => {
   expect(colorButton).toHaveStyle({ backgroundColor: "blue" });
   expect(colorButton).toHaveTextContent("Change to red");
 });
+
+// Initla conditions
+test("check that the button starts out enabled", () => {
+  render(<App />);
+  const colorButton = screen.getByRole("button", { name: "Change to blue" });
+  expect(colorButton).toBeEnabled();
+});
+
+test("check that the checkbox starts out unchecked", () => {
+  render(<App />);
+  const checkBox = screen.getByRole("checkbox");
+
+  expect(checkBox).not.toBeChecked();
+});
+
+test("Checkbox disables button on first click and enables on second click", () => {
+  render(<App />);
+  const checkbox = screen.getByRole("checkbox");
+  const button = screen.getByRole("button");
+
+  fireEvent.click(checkbox);
+  expect(button).toBeDisabled();
+
+  fireEvent.click(checkbox);
+  expect(button).toBeEnabled();
+});
