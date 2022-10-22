f05dae3833385cbe30e5170fa4a5bbb6e5ff09d4
EdwardCraft <eduardo.flores81@uabc.edu.mx>
2022-10-21 18:53:09 -0700

    user event hover and  unhover

--- sundaes-on-demand/src/pages/summary/test/SummaryForm.test.js
+++ sundaes-on-demand/src/pages/summary/test/SummaryForm.test.js
@@ -16,14 +16,38 @@ test("Initial conditions", () => {
 test("Checkbox enables button on first click and disables on second click", async () => {
   const user = userEvent.setup();
   render(<SummaryForm />);
+
   const checkbox = screen.getByRole("checkbox", {
     name: /terms and conditions/i,
   });
   const confirmButton = screen.getByRole("button", { name: /confirm order/i });
 
-  await userEvent.click(checkbox);
+  await user.click(checkbox);
   expect(confirmButton).toBeEnabled();
 
-  await userEvent.click(checkbox);
+  await user.click(checkbox);
   expect(confirmButton).toBeDisabled();
+});
+
+test("Popover response to hover", async () => {
+  const user = userEvent.setup();
+  render(<SummaryForm />);
+
+  //popover start out hidden
+  const nullPopover = screen.queryByText(
+    /no ice cream will actually be delivered/i
+  );
+  expect(nullPopover).not.toBeInTheDocument();
+  //popover appears on mouseover of checkbox label
+  const termsAndConsitions = screen.getByText(/terms and conditions/i);
+
+  await user.hover(termsAndConsitions);
+
+  const popover = screen.getByText(/no ice cream will actually be delivered/i);
+
+  expect(popover).toBeInTheDocument();
+  //popover disappears when we mouse out
+  await user.unhover(termsAndConsitions);
+
+  expect(popover).not.toBeInTheDocument();
 });
