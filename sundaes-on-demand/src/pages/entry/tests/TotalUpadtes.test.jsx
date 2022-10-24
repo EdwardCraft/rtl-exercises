8aee9cb87617f2965859f0866c895927d96928d8
EdwardCraft <eduardo.flores81@uabc.edu.mx>
2022-10-24 16:30:11 -0700

    Test for total updates

---
+++ sundaes-on-demand/src/pages/entry/tests/TotalUpadtes.test.jsx
@@ -0,0 +1,32 @@
+import { render, screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
+
+import Options from "../Options";
+
+it("update scoop subtotal when scoops change", async () => {
+  const user = userEvent.setup();
+  render(<Options optionType="scoops" />);
+
+  // Make sure total starts out 0.00
+  const scoopsSubtotal = screen.getByText("Scoops total: $", { exact: false });
+  expect(scoopsSubtotal).toHaveTextContent("0.00");
+
+  // Update vanillas scoop y to 1 and check the subtotal
+  const vanillaInput = await screen.findByRole("spinbutton", {
+    name: "Vanilla",
+  });
+
+  await user.clear(vanillaInput);
+  await user.type(vanillaInput, "1");
+
+  expect(scoopsSubtotal).toHaveTextContent("2.00");
+  // update chocolate scoops to 2 and check subtotal
+  const chocolateInput = await screen.findByRole("spinbutton", {
+    name: "Chocolate",
+  });
+
+  await user.clear(chocolateInput);
+  await user.type(chocolateInput, "2");
+
+  expect(chocolateInput).toHaveTextContent("6.00");
+});
